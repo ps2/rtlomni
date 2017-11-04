27fb778e500c132fc98c200488ba94dfc2a6185d
F5OEO <evaristec@gmail.com>
2017-11-04 16:49:27 +0100

    Message encapulating in progress

--- rtlomni.c
+++ rtlomni.c
@@ -35,7 +35,7 @@ Licence :
 
 
 
-//#define DEBUG_PACKET 
+#define DEBUG_PACKET 
 
 
 /*
@@ -76,7 +76,7 @@ FILE *DebugFM=NULL;
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-//*************************************DATA DEFINITION***********************************************
+//*************************************DATA DEFINITION AND CRC***********************************************
 unsigned int crc_table16[] = {0x0000,0x8005,0x800f,0x000a,0x801b,0x001e,0x0014,0x8011,0x8033,
                0x0036,0x003c,0x8039,0x0028,0x802d,0x8027,0x0022,0x8063,0x0066,
                0x006c,0x8069,0x0078,0x807d,0x8077,0x0072,0x0050,0x8055,0x805f,
@@ -106,6 +106,197 @@ unsigned int crc_table16[] = {0x0000,0x8005,0x800f,0x000a,0x801b,0x001e,0x0014,0
                0x827f,0x027a,0x826b,0x026e,0x0264,0x8261,0x0220,0x8225,0x822f,
                0x022a,0x823b,0x023e,0x0234,0x8231,0x8213,0x0216,0x021c,0x8219,
                0x0208,0x820d,0x8207,0x0202};
+
+unsigned int crc16(unsigned char *data,int len)
+{
+
+    unsigned int acc = 0x00;
+    for(int i=0;i<len;i++)
+    {
+    
+        acc = (acc >> 8) ^ crc_table16[(acc ^ data[i]) & 0xff];
+    }
+    return acc;
+}
+
+
+/**
+ * \file
+ * Functions and types for CRC checks.
+ *
+ * Generated on Thu Oct 12 16:51:00 2017
+ * by pycrc v0.9.1, https://pycrc.org
+ * using the configuration:
+ *  - Width         = 8
+ *  - Poly          = 0x07
+ *  - XorIn         = 0x00
+ *  - ReflectIn     = False
+ *  - XorOut        = 0x00
+ *  - ReflectOut    = False
+ *  - Algorithm     = table-driven
+ */
+
+static const unsigned char crc_table[256] = {
+    0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15, 0x38, 0x3f, 0x36, 0x31, 0x24, 0x23, 0x2a, 0x2d,
+    0x70, 0x77, 0x7e, 0x79, 0x6c, 0x6b, 0x62, 0x65, 0x48, 0x4f, 0x46, 0x41, 0x54, 0x53, 0x5a, 0x5d,
+    0xe0, 0xe7, 0xee, 0xe9, 0xfc, 0xfb, 0xf2, 0xf5, 0xd8, 0xdf, 0xd6, 0xd1, 0xc4, 0xc3, 0xca, 0xcd,
+    0x90, 0x97, 0x9e, 0x99, 0x8c, 0x8b, 0x82, 0x85, 0xa8, 0xaf, 0xa6, 0xa1, 0xb4, 0xb3, 0xba, 0xbd,
+    0xc7, 0xc0, 0xc9, 0xce, 0xdb, 0xdc, 0xd5, 0xd2, 0xff, 0xf8, 0xf1, 0xf6, 0xe3, 0xe4, 0xed, 0xea,
+    0xb7, 0xb0, 0xb9, 0xbe, 0xab, 0xac, 0xa5, 0xa2, 0x8f, 0x88, 0x81, 0x86, 0x93, 0x94, 0x9d, 0x9a,
+    0x27, 0x20, 0x29, 0x2e, 0x3b, 0x3c, 0x35, 0x32, 0x1f, 0x18, 0x11, 0x16, 0x03, 0x04, 0x0d, 0x0a,
+    0x57, 0x50, 0x59, 0x5e, 0x4b, 0x4c, 0x45, 0x42, 0x6f, 0x68, 0x61, 0x66, 0x73, 0x74, 0x7d, 0x7a,
+    0x89, 0x8e, 0x87, 0x80, 0x95, 0x92, 0x9b, 0x9c, 0xb1, 0xb6, 0xbf, 0xb8, 0xad, 0xaa, 0xa3, 0xa4,
+    0xf9, 0xfe, 0xf7, 0xf0, 0xe5, 0xe2, 0xeb, 0xec, 0xc1, 0xc6, 0xcf, 0xc8, 0xdd, 0xda, 0xd3, 0xd4,
+    0x69, 0x6e, 0x67, 0x60, 0x75, 0x72, 0x7b, 0x7c, 0x51, 0x56, 0x5f, 0x58, 0x4d, 0x4a, 0x43, 0x44,
+    0x19, 0x1e, 0x17, 0x10, 0x05, 0x02, 0x0b, 0x0c, 0x21, 0x26, 0x2f, 0x28, 0x3d, 0x3a, 0x33, 0x34,
+    0x4e, 0x49, 0x40, 0x47, 0x52, 0x55, 0x5c, 0x5b, 0x76, 0x71, 0x78, 0x7f, 0x6a, 0x6d, 0x64, 0x63,
+    0x3e, 0x39, 0x30, 0x37, 0x22, 0x25, 0x2c, 0x2b, 0x06, 0x01, 0x08, 0x0f, 0x1a, 0x1d, 0x14, 0x13,
+    0xae, 0xa9, 0xa0, 0xa7, 0xb2, 0xb5, 0xbc, 0xbb, 0x96, 0x91, 0x98, 0x9f, 0x8a, 0x8d, 0x84, 0x83,
+    0xde, 0xd9, 0xd0, 0xd7, 0xc2, 0xc5, 0xcc, 0xcb, 0xe6, 0xe1, 0xe8, 0xef, 0xfa, 0xfd, 0xf4, 0xf3
+};
+
+
+unsigned char crc_8(unsigned char crc, const void *data, size_t data_len)
+{
+    const unsigned char *d = (const unsigned char *)data;
+    unsigned int tbl_idx;
+
+    while (data_len--) {
+        tbl_idx = crc ^ *d;
+        crc = crc_table[tbl_idx] & 0xff;
+        d++;
+    }
+    return crc & 0xff;
+};
+
+
+//***************************************************************************************************
+//*********************************** TRANSMISSION PART *********************************************
+//***************************************************************************************************
+#define MAX_BYTE_PER_PACKET 33
+#define MAX_PACKETBYMESSAGE 1000
+typedef struct TxPacketRF
+{
+    unsigned char PacketBufferRF[MAX_BYTE_PER_PACKET];
+    unsigned int PhysicaAddress;
+    unsigned char Type;
+    unsigned char Sequence;
+    int PacketLength;
+} TxPacketRF;
+
+
+
+
+//********************* Packet Layer
+
+int WaitForAcknowledge();
+int SendAcknowledge(int PacketSeq);
+
+ // *********** Physical packet header ************************
+
+int SetPacket(TxPacketRF *PacketRF,unsigned char *Body,int Length)
+{
+    PacketRF->PacketLength=0;
+    // Physical address
+    PacketRF->PacketBufferRF[PacketRF->PacketLength++]=PacketRF->PhysicaAddress>>24;
+    PacketRF->PacketBufferRF[PacketRF->PacketLength++]=PacketRF->PhysicaAddress>>16;
+    PacketRF->PacketBufferRF[PacketRF->PacketLength++]=PacketRF->PhysicaAddress>>8;
+    PacketRF->PacketBufferRF[PacketRF->PacketLength++]=PacketRF->PhysicaAddress&0xFF;
+    //Type = PDM , Sequence should be updated     
+    PacketRF->PacketBufferRF[PacketRF->PacketLength++]=(PacketRF->Type<<5)|(PacketRF->Sequence&0x1F);
+    int ByteSent=0;
+
+    if(Length>(MAX_BYTE_PER_PACKET-6)) //6 is the physical packet overhead
+    {  
+        memcpy(PacketRF->PacketBufferRF+5,Body,MAX_BYTE_PER_PACKET-6);
+        PacketRF->PacketLength+=MAX_BYTE_PER_PACKET-6;
+  
+        PacketRF->PacketBufferRF[MAX_BYTE_PER_PACKET-1]=crc_8(0,PacketRF->PacketBufferRF,MAX_BYTE_PER_PACKET-1);
+        ByteSent=MAX_BYTE_PER_PACKET-6;  
+    }
+    else
+    {
+        memcpy(PacketRF->PacketBufferRF+5,Body,Length);
+        PacketRF->PacketLength+=Length;
+        PacketRF->PacketBufferRF[PacketRF->PacketLength-1]=crc_8(0,PacketRF->PacketBufferRF,PacketRF->PacketLength-1);
+        ByteSent=Length;
+    }
+    return ByteSent;
+    
+}
+
+
+// ******************** Message Layer **************
+#define MAX_BYTE_MESSAGE 4000
+
+typedef struct TxMessage
+{
+    TxPacketRF TxPoolBuffer[MAX_PACKETBYMESSAGE];
+    int NbPacket;
+
+    unsigned int Address;
+     
+    unsigned char Body[MAX_BYTE_MESSAGE];
+    unsigned int BodyLength;
+   unsigned char Sequence;
+//private:
+    unsigned char CompleteMessage[MAX_BYTE_MESSAGE];
+    
+} TxMessage;
+ 
+
+int PacketizeMessage(TxMessage *Message,int Ack,int TimeOut)
+{
+    
+    Message->NbPacket=0;
+
+    //Need a CRC16 at end
+    memcpy(Message->CompleteMessage+6,Message->Body,Message->BodyLength);
+
+    Message->CompleteMessage[0]=Message->Address>>24;
+    Message->CompleteMessage[1]=Message->Address>>16;
+    Message->CompleteMessage[2]=Message->Address>>8;
+    Message->CompleteMessage[3]=Message->Address&0xFF;
+    
+    Message->CompleteMessage[4]=(Message->Sequence<<2)|((Message->BodyLength>>8)&0x3);
+    Message->CompleteMessage[5]=Message->BodyLength&0xFF;
+
+    unsigned int Messagecrc=crc16(Message->CompleteMessage,Message->BodyLength+6);
+    Message->CompleteMessage[Message->BodyLength+6]=Messagecrc>>8;
+    Message->CompleteMessage[Message->BodyLength+6+1]=Messagecrc&0xFF;
+    
+    int MessageLengthRemaining=Message->BodyLength+6+2;
+    int ByteSent=0;
+    do
+    {
+        TxPacketRF *PacketRF=&Message->TxPoolBuffer[Message->NbPacket];
+        PacketRF->PhysicaAddress=Message->Address; // FixMe here @physical == @Message which is not the case for pairing
+        PacketRF->Sequence=(0/* Last packet sequence*/+2)&0x1F;
+        if(Message->NbPacket==0)
+            PacketRF->Type=PDM;
+        else
+            PacketRF->Type=CON;
+
+        int ByteSentPacket=SetPacket(PacketRF,&Message->CompleteMessage[ByteSent],MessageLengthRemaining);
+        ByteSent+=ByteSentPacket;
+        MessageLengthRemaining-=ByteSentPacket;
+    }
+    while (MessageLengthRemaining>0);
+
+    return 1;
+}
+int TxAddPacket(unsigned int PhysAddress,int PacketSeq,int Type,unsigned char *Body);
+    
+//********************* Message Layer
+int TxAddMessage();
+
+
+int TxAddSubMessage();
+
+//
+
+
+
 
 //***************************************************************************************************
 //*********************************** NONCE GENERATION***********************************************
@@ -272,7 +463,7 @@ unsigned char printbit(unsigned char Byte,int bitstart,int bitstop)
 
 void InterpretSubMessage(int Source,int Type,unsigned char *SubMessage,int Length,int SeqMessage)
 {
-    enum {Cmd_GetConfig=3,Cmd_Pairing=7,Cmd_GetStatus=0xe,Cmd_BasalScheduleExtra=0x13,Cmd_InsulinScheduleExtra=0x17,Cmd_SyncTime=0x19,Cmd_InsulinSchedule=0x1a,Cmd_CancelBolus=0x1F,Cmd_CancelPOD=0x1C};
+    enum {Cmd_GetConfig=3,Cmd_Pairing=7,Cmd_GetStatus=0xe,Cmd_BasalScheduleExtra=0x13,Cmd_InsulinScheduleExtra=0x17,Cmd_SyncTime=0x19,Cmd_InsulinSchedule=0x1a,Cmd_CancelBolus=0x1F,Cmd_CancelPOD=0x1C,Cmd_Diagnose=0x1E};
     enum {Resp_Status=0x1D,Resp_Tid=0x01,Resp02=0x02,RespError=0x06};
     const char *TypeInsulin[]={"Basal","Temp Basal","Bolus","All","All","All","All","All","All"};
 
@@ -424,7 +615,20 @@ void InterpretSubMessage(int Source,int Type,unsigned char *SubMessage,int Lengt
             
         }
         break;
-
+        case Cmd_Diagnose:
+        {
+            printf("Diagnose ");
+            for(int i=0;i<Length;i++) printf("%02x",SubMessage[i]);
+            printf(" ");
+            switch(SubMessage[0]&0xF)
+            {       
+                case 0x2: printf("Beeeeep Beep Beeeeep Beep");break;
+                case 0xE: printf("Beeeeeeeeeeep");break;
+                default: printf("Unknown function");
+            }
+                
+        }
+        break;
         case Resp_Status: 
         //https://github.com/openaps/openomni/wiki/Status-response-1D
         /*The 1D response has the following form:
@@ -620,17 +824,7 @@ void ParseSubMessage(int Seq,int Source,unsigned char *Message,int Length,int Se
 
 
 
-unsigned int crc16(unsigned char *data,int len)
-{
 
-    unsigned int acc = 0x00;
-    for(int i=0;i<len;i++)
-    {
-    
-        acc = (acc >> 8) ^ crc_table16[(acc ^ data[i]) & 0xff];
-    }
-    return acc;
-}
 
 
 
@@ -717,54 +911,7 @@ void AddMessage(int Seq,int Source,unsigned char*Packet,int Length,int TargetMes
 //***************************************************************************************************
 
 
-/**
- * \file
- * Functions and types for CRC checks.
- *
- * Generated on Thu Oct 12 16:51:00 2017
- * by pycrc v0.9.1, https://pycrc.org
- * using the configuration:
- *  - Width         = 8
- *  - Poly          = 0x07
- *  - XorIn         = 0x00
- *  - ReflectIn     = False
- *  - XorOut        = 0x00
- *  - ReflectOut    = False
- *  - Algorithm     = table-driven
- */
 
-static const unsigned char crc_table[256] = {
-    0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15, 0x38, 0x3f, 0x36, 0x31, 0x24, 0x23, 0x2a, 0x2d,
-    0x70, 0x77, 0x7e, 0x79, 0x6c, 0x6b, 0x62, 0x65, 0x48, 0x4f, 0x46, 0x41, 0x54, 0x53, 0x5a, 0x5d,
-    0xe0, 0xe7, 0xee, 0xe9, 0xfc, 0xfb, 0xf2, 0xf5, 0xd8, 0xdf, 0xd6, 0xd1, 0xc4, 0xc3, 0xca, 0xcd,
-    0x90, 0x97, 0x9e, 0x99, 0x8c, 0x8b, 0x82, 0x85, 0xa8, 0xaf, 0xa6, 0xa1, 0xb4, 0xb3, 0xba, 0xbd,
-    0xc7, 0xc0, 0xc9, 0xce, 0xdb, 0xdc, 0xd5, 0xd2, 0xff, 0xf8, 0xf1, 0xf6, 0xe3, 0xe4, 0xed, 0xea,
-    0xb7, 0xb0, 0xb9, 0xbe, 0xab, 0xac, 0xa5, 0xa2, 0x8f, 0x88, 0x81, 0x86, 0x93, 0x94, 0x9d, 0x9a,
-    0x27, 0x20, 0x29, 0x2e, 0x3b, 0x3c, 0x35, 0x32, 0x1f, 0x18, 0x11, 0x16, 0x03, 0x04, 0x0d, 0x0a,
-    0x57, 0x50, 0x59, 0x5e, 0x4b, 0x4c, 0x45, 0x42, 0x6f, 0x68, 0x61, 0x66, 0x73, 0x74, 0x7d, 0x7a,
-    0x89, 0x8e, 0x87, 0x80, 0x95, 0x92, 0x9b, 0x9c, 0xb1, 0xb6, 0xbf, 0xb8, 0xad, 0xaa, 0xa3, 0xa4,
-    0xf9, 0xfe, 0xf7, 0xf0, 0xe5, 0xe2, 0xeb, 0xec, 0xc1, 0xc6, 0xcf, 0xc8, 0xdd, 0xda, 0xd3, 0xd4,
-    0x69, 0x6e, 0x67, 0x60, 0x75, 0x72, 0x7b, 0x7c, 0x51, 0x56, 0x5f, 0x58, 0x4d, 0x4a, 0x43, 0x44,
-    0x19, 0x1e, 0x17, 0x10, 0x05, 0x02, 0x0b, 0x0c, 0x21, 0x26, 0x2f, 0x28, 0x3d, 0x3a, 0x33, 0x34,
-    0x4e, 0x49, 0x40, 0x47, 0x52, 0x55, 0x5c, 0x5b, 0x76, 0x71, 0x78, 0x7f, 0x6a, 0x6d, 0x64, 0x63,
-    0x3e, 0x39, 0x30, 0x37, 0x22, 0x25, 0x2c, 0x2b, 0x06, 0x01, 0x08, 0x0f, 0x1a, 0x1d, 0x14, 0x13,
-    0xae, 0xa9, 0xa0, 0xa7, 0xb2, 0xb5, 0xbc, 0xbb, 0x96, 0x91, 0x98, 0x9f, 0x8a, 0x8d, 0x84, 0x83,
-    0xde, 0xd9, 0xd0, 0xd7, 0xc2, 0xc5, 0xcc, 0xcb, 0xe6, 0xe1, 0xe8, 0xef, 0xfa, 0xfd, 0xf4, 0xf3
-};
-
-
-unsigned char crc_8(unsigned char crc, const void *data, size_t data_len)
-{
-    const unsigned char *d = (const unsigned char *)data;
-    unsigned int tbl_idx;
-
-    while (data_len--) {
-        tbl_idx = crc ^ *d;
-        crc = crc_table[tbl_idx] & 0xff;
-        d++;
-    }
-    return crc & 0xff;
-};
 
 void ParsePacket(unsigned int TimePacket) //TimePacket is in millisecond
 {
@@ -1240,7 +1387,8 @@ int main(int argc, char*argv[])
    
    
 
-  
+  mlot=43080;
+    mtid=480590;    
 
     
     if(argc>=2)
@@ -1281,6 +1429,7 @@ int main(int argc, char*argv[])
  // InitNounce(43080,480653,0); // Date 23/10 - 26/10
    // InitNounce(43080,430528,0);//26/10- 30/10
     //InitNounce(43080,420524,0);//30/10- 2/11
+   // InitNounce(43080,420590,0);//2/11- 5/11
      InitNounce(mlot,mtid,0);   
       
    InitRF();
